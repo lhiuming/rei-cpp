2761f0aac1d34e7b95da08c57665424d1bf7aee8
Syaoming <syaoming13@gmail.com>
2017-04-17 21:08:32 +0800

    tested with cmake system

--- src/graphics.cpp
+++
@@ -1,4 +0,0 @@
-/**
- * This is supposed to be a Wrapper for OpenGL via GLFW.
- */
-#include <GLFW/glfw3.h>
---
+++ tests/play_with_glfw.cpp
@@ -0,0 +1,11 @@
+//#include <GLFW/glfw2.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello, world! with OpenGL" << endl;
+
+  return 0;
+}
