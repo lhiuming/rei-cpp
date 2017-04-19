93a28bf50a9ee64e3e2ac345616b6edf6171fedd
Syaoming <syaoming13@gmail.com>
2017-04-19 22:04:09 +0800

    still cannot draw a array on the window

--- tests/play_with_glfw.cpp
+++ tests/play_with_glfw.cpp
@@ -15,12 +15,123 @@ void error_callback(int error, const char* description)
     cerr << "Error: " << description << endl;
 }
 
-void update(size_t width, size_t height, unsigned char *pixels)
-{
+GLuint VAO;
+GLuint Buffer; // vertice coordinates buffer id
+GLuint Texture; // triangle texture buffer id
+GLuint Sampler; // sampler id; should be related to the Texture unit
 
-  // find a way to draw a buffer to the screen 
+
+// this "pass-throuhg" shader comes from the red book
+static const char* vertex_shader_text =
+"#version 410 core\n"
+"layout (location = 0) in vec2 in_position;\n"
+"layout (location = 1) in vec2 in_tex_coord;\n"
+"out vec2 vs_tex_coord;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(in_position, 0, 1.0);\n"
+"    vs_tex_coord = in_tex_coord;\n"
+"}\n";
+
+
+// this shader comes from the red book
+static const char* fragment_shader_text =
+"#version 410 core\n"
+"in vec2 vs_tex_coord;\n"
+"layout (location = 0) out vec3 color;\n"
+"uniform sampler2D tex;\n"
+"void main()\n"
+"{\n"
+"    //color = vec4(0, 0.7, 0.6, 0.5);\n"
+"    color = texture(tex, vs_tex_coord).rgb;\n"
+"    //color = vec4(0, 0.7, 0.6, 0.5).rgb;\n"
+"}\n";
+
+
+void init()
+{
+  // intialize the window with two triangle and a texture
+
+  // defaut color set to asuka orange
+  glClearColor(0.9, 0.4, 0.0, 1.0);
+
+  // create triangle array objects
+  glGenVertexArrays(1, &VAO); // retrive a VAO id
+  glBindVertexArray(VAO); // allocate memory to the VAO, and focus GL on it
+
+  // specify the vertices
+  GLfloat two_triangle_data[] = {
+    // vertex positions
+    -0.9, -0.9, -0.9, 0.9, 0.9, 0.9, // top-left
+    0.9, 0.9, 0.9, -0.9, -0.9, -0.9,  // bottom-right
+    // vertex texture coordinates
+    0, 0,  0, 1,  1, 1,
+    1, 1,  1, 0,  0, 0,
+  };
+
+  // create the buffer for vertex data
+  glGenBuffers(1, &Buffer);  // retrive a buffer id
+  glBindBuffer(GL_ARRAY_BUFFER, Buffer);  // create a buffer object
+  glBufferData(GL_ARRAY_BUFFER, sizeof(two_triangle_data),  // allocate and initialize
+               two_triangle_data, GL_STATIC_DRAW);          // the buffer
+
+  // set vertex attributes
+  // first part is position
+  glVertexAttribPointer(0,  // vPosition is with location 0
+                        2,  // each vertex take two data (as position)
+                        GL_FLOAT, // type of data each vertex takes
+                        GL_FALSE, // ask GL not to normalize the data
+                        0,  // stride; 0 means read buffer side-by-side/no-skip
+                        (GLvoid *)0); // read from the start of the buffer
+  glEnableVertexAttribArray(0); // enable the input data of id 0
+  // second part is texture coordinates
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0,
+    (GLvoid *)(12 * sizeof(float)));
+  glEnableVertexAttribArray(1); // enable the input data of id 1
+
+  // create a texture object
+  glGenTextures(1, &Texture);  // retrive a texture object id
+  glBindTexture(GL_TEXTURE_2D, Texture);  // set the type of the texture
+
+  // allocate memory for the texture
+  glTexStorage2D(
+    GL_TEXTURE_2D, // is 2D
+    0, // no real mipmap
+    GL_RGBA8UI, WINDOW_W, WINDOW_H);
 
 }
+
+void update(GLubyte pixels[WINDOW_W * WINDOW_H * 4], GLuint prog)
+{
+  // Draw pixels to the screen
+
+  // clear buffer before filling
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  // re-load the shader program ?
+  glUseProgram(prog);
+
+  // update the pixels as texture on the window triangles
+  glTexSubImage2D(
+    GL_TEXTURE_2D, // as the set up above in init()
+    0, // mipmap level
+    0, 0, // x and y offset
+    WINDOW_W, WINDOW_H, // texture dimension
+    GL_RGBA_INTEGER, // texture format; we use 8-bit byte
+    GL_UNSIGNED_BYTE, // data type
+    pixels  // texture data
+   );
+
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+  // draw the triangle, now with new texture
+  glBindVertexArray(VAO);  // feed the VAO id; now GL focus on it
+  glDrawArrays(GL_TRIANGLES, 0, 6);  // ask GL to draw triangle from the vertices
+
+  glFlush(); // send all commands
+}
+
 
 int main() {
 
@@ -32,8 +143,15 @@ int main() {
   }
   glfwSetErrorCallback(error_callback);
 
-  // create a window
-  GLFWwindow* window = glfwCreateWindow(WINDOW_W, WINDOW_H, "My Test Window", NULL, NULL);
+  // create a window; Most of them is some what necessary, at leat in macOS
+  //glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL
+
+  GLFWwindow* window = glfwCreateWindow(WINDOW_W, WINDOW_H,
+     "My Test Window", NULL, NULL);
   if (!window) {
     // Window or OpenGL context creation failed
     cout << "GLFW window creation faile!" << endl;
@@ -44,7 +162,7 @@ int main() {
   glfwMakeContextCurrent(window);
   glfwSwapInterval(1);
 
-  // initialze GLEW; after this, we can use GL core and extension functionality
+  // initialze GLEW; after this, we can use GL core and extension
   GLenum glew_err = glewInit();
   if (glew_err != GLEW_OK) {
     // GLEW intialization failed
@@ -61,18 +179,37 @@ int main() {
 
 
   // create a gray buffer
-  unsigned char pixels[WINDOW_W * WINDOW_H * 4];
-  for (size_t i = 0; i < WINDOW_W; ++i)
-    for (size_t j = 0; j < WINDOW_H; ++j) {
-      ; // just show the random data from memory
+  GLubyte pixels[WINDOW_W * WINDOW_H * 4];
+  for (size_t i = 0; i < WINDOW_H; ++i)
+    for (size_t j = 0; j < WINDOW_W; ++j) {
+      pixels[i * WINDOW_H + j    ] = (GLubyte) 128;
+      pixels[i * WINDOW_H + j + 1] = (GLubyte) 128;
+      pixels[i * WINDOW_H + j + 2] = (GLubyte) 128;
+      pixels[i * WINDOW_H + j + 3] = (GLubyte) 128;
     }
+
+  init();
+
+  // load two pass-through shader
+  GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+  glCompileShader(vertex_shader);
+
+  GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+  glCompileShader(fragment_shader);
+
+  GLuint program = glCreateProgram();
+  glAttachShader(program, vertex_shader);
+  glAttachShader(program, fragment_shader);
+  glLinkProgram(program);
 
   // Keep running when the window is not "closed" by user
   while (!glfwWindowShouldClose(window)){
     // Keep running
-    update(720, 480, pixels);
-
+    update(pixels, program);
     glfwSwapBuffers(window);
+    glfwPollEvents();  // This is necessary !!!
   }
 
   cout << "Window closed. " << endl;
