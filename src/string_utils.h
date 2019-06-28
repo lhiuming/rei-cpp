367f2893e17f533a15cbdcf17ea28d37b4e71c2f
lhiuming <laaihiuming@gmail.com>
2019-06-29 00:32:01 +0800

    Add string convertion and fix bug

--- src/string_utils.h
+++ src/string_utils.h
@@ -14,10 +14,22 @@ static inline std::wostream& operator<<(std::wostream& wos, std::string str) {
 
 static inline std::wstring make_wstring(const char* c_str) {
   std::wstring ret;
-  while (&c_str != '\0') {
+  while (*c_str != '\0') {
     ret.push_back(wchar_t(*c_str));
+    c_str++;
   }
   return ret;
+}
+
+static inline int to_cstr(const wchar_t* wchars, char* buffer) {
+  int i = 0;
+  while (*wchars != '\0') {
+    // TODO this will overflow
+    buffer[i++] = char(*wchars);
+    wchars++;
+  }
+  buffer[i++] = '\0';
+  return i;
 }
 
 } // namespace rei
