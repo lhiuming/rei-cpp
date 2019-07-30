6d94b97877f27a8b52219c919f824a0db14acbdf
lhiuming <laaihiuming@gmail.com>
2019-07-30 12:32:35 +0800

    Update ryatrace demo

--- src/direct3d/shader/procedural_env.hlsl
+++ src/direct3d/shader/procedural_env.hlsl
@@ -5,9 +5,9 @@
 float3 gradiant_sky_eva(float3 view_direction) {
   float3 ayanami_blue = {129.0 / 255, 187.0 / 255, 235.0 / 255};
   float3 asuka_red = {156.0 / 255, 0, 0};
-  float y = view_direction.y;
-  float mixing = clamp(1 - y, 0, 2) / 2;
-  mixing = pow(mixing, 3);
+  float y = 1 - view_direction.y;
+  float mixing = saturate((y + 0.25) / 2);
+  mixing = pow(mixing, 5);
   return lerp(ayanami_blue, asuka_red, mixing);
 }
 
